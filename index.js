4bbf8e040aeb1e06c7d27fab4cb3efb05ede9e87
Jennifer Proust <jennifer.proust@gmail.com>
2015-03-05 12:25:25 +0100

    Demo push local changes

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
+var cool = require("cool-ascii-faces");
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(cool());
 });
+// app.get('/', function(request, response) {
+//   response.send('Hello World!');
+// });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
