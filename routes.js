319dc203d7e7c0ab83fb64defba0b83690fc73aa
Proustibat <jennifer.proust@gmail.com>
2015-03-08 09:17:24 +0100

    configuring passport for local account and handling registration

--- routes.js
+++ routes.js
@@ -79,9 +79,12 @@ module.exports = function(app, passport, router, data) {
             message: req.flash('signupMessage')
         });
     });
-    // process the signup form
-    // app.post('/signup', do all our passport stuff here);
-
+     // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
     // =====================================
     // PROFILE SECTION =====================
