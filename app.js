821197831e1066af83f9fe1bfe6aee9f3a083d85
Proustibat <jennifer.proust@gmail.com>
2015-03-08 08:59:48 +0100

    remove step by step instructions to debug heroku online app

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ require('./config')(app);
 
 // required for passport
 app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
-// app.use(passport.initialize());
-// app.use(passport.session()); // persistent login sessions
-// app.use(flash()); // use connect-flash for flash messages stored in session
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 
 // ROUTES
