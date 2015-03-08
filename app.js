c25b110f9529cecae162a38bb62609464ba0fc30
Proustibat <jennifer.proust@gmail.com>
2015-03-08 08:39:03 +0100

    remove step by step instructions to debug heroku online app

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var flash = require('connect-flash');
 
-// var morgan = require('morgan');
+var morgan = require('morgan');
 // var cookieParser = require('cookie-parser');
 // var bodyParser = require('body-parser');
 // var session = require('express-session');
