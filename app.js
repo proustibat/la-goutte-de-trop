5d93f2022fab105b0869fb52c6e1cace03b9f583
Proustibat <jennifer.proust@gmail.com>
2015-03-08 08:37:04 +0100

    remove step by step instructions to debug heroku online app

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
-var configDB = require('./config/database.js');
+// var configDB = require('./config/database.js');
 
 // CONFIGURATION
 // mongoose.connect(configDB.url); // connect to our database
