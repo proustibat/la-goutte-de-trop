503e2a3cb59af3b52510e31d5dc9dfe3d8758689
Proustibat <jennifer.proust@gmail.com>
2015-03-08 05:05:24 +0100

    try to get protocol for heroku app deployed

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 (function($) {
     // var socket = io.connect('/');
-    var socket = io.connect('http://'+window.location.hostname+':'+window.location.port);
+    var socket = io.connect(window.location.protocol+'//'+window.location.hostname+':'+window.location.port);
 
     socket.on('connect', function() {
         console.log('connect');
