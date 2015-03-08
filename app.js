37e03fbbac66878fefde85a410d9c348d79c593d
Proustibat <jennifer.proust@gmail.com>
2015-03-08 16:39:51 +0100

    twitter application

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ var bodyParser = require('body-parser');
 var configDB = require('./config/database.js');
 // var globalData = require('./data.js');
 
+// CONFIGURATION ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+require('./config/passport')(passport); // pass passport for configuration
+require('./config')(app);
+
 // SESSION & PASSPORT ======================================================================
 app.use(session({
     genid: function(req) {
@@ -27,15 +32,10 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
-// CONFIGURATION ===============================================================
-mongoose.connect(configDB.url); // connect to our database
-require('./config/passport')(passport); // pass passport for configuration
-require('./config')(app);
-
 
 // ROUTES ======================================================================
-require('./routes')(app, passport, router);
 app.use('/', router);
+require('./routes')(app, passport, router);
 
 // LAUNCH ======================================================================
 var server = app.listen(app.get('port'), function() {
