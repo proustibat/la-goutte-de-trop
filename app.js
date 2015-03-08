97760fe7001bd4a91ce147de9047905bf3922d79
Proustibat <jennifer.proust@gmail.com>
2015-03-08 01:22:36 +0100

    Basic server with simple templating

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
+// Require
 var express = require('express');
+
+// Express
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+var port = process.env.PORT || 5000;
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/templates');
+app.set('port', port);
 app.use(express.static(__dirname + '/public'));
 
 
+
+// Configure
+// app.use(express.static(__dirname + '/public'));
+// app.set('port', (process.env.PORT || 5000));
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World !!');
+    response.render('index');
 });
+
+// app.use(function(err, req, res, next) {
+//     console.error(err.stack);
+//     res.status(500).send('Something broke!');
+// });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+    console.log("Node app is running at localhost:" + app.get('port'));
 });
-
-
---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+(function($) {
+
+    // var socket = io.connect(window.location.hostname);
+
+    // socket.on('status', function(data) {
+    //     $('#status').html(data.status);
+    // });
+
+    // $('#reset').on('click', function(e) {
+    //     socket.emit('reset');
+    // })
+
+})(jQuery);
